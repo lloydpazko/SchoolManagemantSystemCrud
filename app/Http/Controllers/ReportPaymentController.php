b305c61644e7aef0d73399d5a5010e5e6e797829
Lloyd P Paliuanan <lloydpazko@gmail.com>
2024-01-28 00:35:45 +0800

    fix report controller

--- app/Http/Controllers/ReportPaymentController.php
+++ app/Http/Controllers/ReportPaymentController.php
@@ -10,30 +10,31 @@ class ReportPaymentController extends Controller
 {
     public function report1($pid){
 
-        $payments = Payment::find($pid);
+        $PaymentSystemReleaseReceipt = Payment::find($pid);
         $pdf = App::make('dompdf.wrapper');
-        $print = "<div style='margin:20px; padding:20px;'>";
-        $print = "<h1 align='center'> Payment Recipt </h1>";
+        $print = "<div style='margin:20px; padding:20px'>";
+        $print = "<h1> Payment Receipt </h1>";
         $print = "<hr/>";
-        $print = "<p> Recipt No: <b>". $pid . " </b></p>";
-        $print = "<p> Date: <b>". $payments->paid_date . "</b></p>";
-        $print = "<p> Enrollment: <b>". $payments->EnrollmentSystem->enroll_no . "</b></p>";
-        $print = "<p> Student Name : <b>". $payments->EnrollmentSystem->Students->name . "</b></p>";
+        $print = "<p> Recipt No: <b>" . $pid . " </b></p>";
+        $print = "<p> Date: <b>" . $PaymentSystemReleaseReceipt->paid_date . "</b></p>";
+        $print = "<p> Enrollment: <b>" . $PaymentSystemReleaseReceipt->enrollment->enroll_no . "</b></p>";
+        $print = "<p> Student Name : <b>" . $PaymentSystemReleaseReceipt->enrollment->student->name . "</b></p>";
         $print = "<hr/>";
+
+        //creation table
         $print = "<table style='width:100%'>";
         $print = "<tr>";
         $print = "<td> Description </td>";
         $print = "<td>Amount<td>";
         $print = "</tr>";
 
-        $print = "<td> <h3>" . $payments->EnrollmentSystem->Batch_name . "</h3> </td>";
-        $print = "<td> <h3>" . $payments->amount . "</h3> </td>";
+        $print = "<td> <h3>" . $PaymentSystemReleaseReceipt->enrollment->Batch_name . "</h3> </td>";
+        $print = "<td> <h3>" . $PaymentSystemReleaseReceipt->amount . "</h3> </td>";
         $print = "</tr>";
 
         $print = "</table>";
         $print = "<hr/>";
-        $print = "<span> Printed by: <b>". Auth::user()->name ."</b> </span>";
-        $print = "<span> Printed date: <b>". date('y-m-d') ."</b> </span>";
+        $print = "<span> Printed date: <b>" . date('y-m-d') . "</b> </span>";
         $print = "</div>";
         $pdf->loadHTML( $print);
         return $pdf->stream();
